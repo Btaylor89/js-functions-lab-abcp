17005983bf4f8c1c242c2d64c26b1dcd6eec2426
Britni Chavis <brit.chavis@gmail.com>
2018-01-16 02:07:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 function happyHolidays() {
   return "Happy holidays!"
 }
-function happyHolidaysTo(name) {
-  return `Happy holidays, ${name}!`
+function happyHolidayTo(name) {
+  return `Happy holiday, ${name}!`
 }
 function holidayCountdown(days , holiday) {
   return `It's ${days} days until ${holiday}!`
